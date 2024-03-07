0ec947c46afb44536fca08eeca9fd73682dfecb9
nikolaprolic <nikolatepic450@gmail.com>
2024-03-07 19:31:54 +0100

    zadatak1

--- OTS2023_ConventorApp/Form1.cs
+++ OTS2023_ConventorApp/Form1.cs
@@ -16,5 +16,46 @@ namespace OTS2023_ConventorApp
         {
             InitializeComponent();
         }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Pounds";
+            label3.Text = "Kg";
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Feet";
+            label3.Text = "m";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                float weight = float.Parse(textBox1.Text);
+                ConvertToKg(weight);
+            }
+            else if (radioButton2.Checked)
+            {
+                float height = float.Parse(textBox1.Text);
+                ConvertToM(height);
+            }
+        }
+
+        public void ConvertToKg(float pounds)
+        {
+            double kg = pounds * 0.4;
+            textBox2.Text = kg.ToString();
+        }
+
+        public void ConvertToM(float feet)
+        {
+            double m = feet * 0.3;
+            textBox2.Text = m.ToString();
+        }
+
     }
 }
